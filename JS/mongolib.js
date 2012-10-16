f04e3f1afdb49917083fcc36630490595faf3e7a
Ben Ferree <ben.ferree@gmail.com>
2012-10-15 23:40:58 -0500

    Mongo REST api post

---
+++ JS/mongolib.js
@@ -0,0 +1,16 @@
+mongolocation = "https://api.mongolab.com/api/1/databases/gymchamp/collections/user?apiKey=507423c4e4b088be4c29ee26";
+function validateuser(){
+
+}
+
+function get_user_info(email){
+
+
+
+
+
+}
+
+function get_friend_info(){
+
+}
