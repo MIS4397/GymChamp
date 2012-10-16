2d04ccd9af0983822a7f5e588af051c64bf6feb2
Ben Ferree <ben.ferree@gmail.com>
2012-10-16 11:47:35 -0500

    Working on mongolib.js functions
    XMLHttpRequest giving me some issues but I think I got it sorted out

--- JS/mongolib.js
+++ JS/mongolib.js
@@ -1,16 +1,34 @@
-mongolocation = "https://api.mongolab.com/api/1/databases/gymchamp/collections/user?apiKey=507423c4e4b088be4c29ee26";
+mongolocation = "https://api.mongolab.com/api/1/databases/gymchamp/collections/user?";
+key = "apiKey=507423c4e4b088be4c29ee26";
 function validateuser(){
 
 }
 
 function get_user_info(email){
-
-
-
-
-
+json = mongoget(email);
 }
 
-function get_friend_info(){
+function get_friend_info(uid){
+json = mongoget(uid);
+}
 
+function mongoget(query){
+var stuff = new XMLHttpRequest();
+req = mongolocation + "q=" + query + key;
+stuff.open("GET", req);
+stuff.setRequestHeader("Content-Type", "application/json");
+stuff.send(null);
+return stuff.response;
+}
+function mongopost(json){
+var stuff = new XMLHttpRequest();
+stuff.open("POST", "https://api.mongolab.com/api/1/databases/gymchamp/collections/user?apiKey=507423c4e4b088be4c29ee26");
+stuff.setRequestHeader("Content-Type", "application/json");
+stuff.send(json);
+}
+function mongoput(json){
+var stuff = new XMLHttpRequest();
+stuff.open("PUT", "https://api.mongolab.com/api/1/databases/gymchamp/collections/user?apiKey=507423c4e4b088be4c29ee26");
+stuff.setRequestHeader("Content-Type", "application/json");
+stuff.send(json);
 }
